8aac4644c4920ac9dd4da998802707709d809f60
elizabeth <ellizzabbetth@gmail.com>
2023-03-25 08:38:28 -0400

    config postgresql

--- app/app/settings.py
+++ app/app/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # values pulled from docker-compse config in app/environment
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 

dd9894072962aeb16af2ea85dde5e4b4a701f4d4
elizabeth <ellizzabbetth@gmail.com>
2023-03-28 06:19:49 -0400

    navigation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-//import './index.scss';
+import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 

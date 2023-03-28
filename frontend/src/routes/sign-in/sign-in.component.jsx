39717a89486e295f275a7f2848fb5089b4c27c93
elizabeth <ellizzabbetth@gmail.com>
2023-03-28 06:31:49 -0400

    sign in

---
+++ frontend/src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,20 @@
+// import {
+//     signInWithGooglePopup,
+//     createUserProfileDocument,
+//   } from '../../utils/firebase/firebase.utils';
+  
+  const SignIn = () => {
+    // const logGoogleUser = async () => {
+    //   const response = await signInWithGooglePopup();
+    //   createUserProfileDocument(response);
+    // };
+    console.log('signin')
+    return (
+      <div>
+        <h1>Sign In Page</h1>
+        <button onClick={() => {}}>Sign in with Google Popup</button>
+      </div>
+    );
+  };
+  
+  export default SignIn;

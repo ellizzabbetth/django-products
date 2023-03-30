1b03c431aefaab31b524788ca36e2c30003168eb
elizabeth <ellizzabbetth@gmail.com>
2023-03-30 14:45:16 -0400

    add item to cart

--- frontend/src/contexts/cart.context.jsx
+++ frontend/src/contexts/cart.context.jsx
@@ -1,12 +1,42 @@
 import { createContext, useState } from 'react';
 
+// Always return new array with new object. No mutations.
+const addCartItem = (cartItems, productToAdd) => {
+    // find if cartItems contains productToAdd
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+        );
+
+    // If found, increment quantity and return.
+    if (existingCartItem) {
+        return cartItems.map((cartItem) =>
+        cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+        );
+    }
+        
+    // return new array with modified cartItems and/or new cart item
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+  };
+
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {}
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd))
+  }
+
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
